e507b31cbe5c08a14517d679e8687c83f45dc6dd
irene <irene277299@gmail.com>
2017-10-24 00:11:15 +0800

    chore: add user info in wish detail page

--- app/screens/WishDetail.js
+++ app/screens/WishDetail.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import {
 	Animated,
 	Image,
-	ScrollView,
-	StyleSheet,
 	Text,
 	View,
+	TouchableOpacity,
 } from 'react-native';
 import Comments from '../components/Comments';
 
 const defaultSource = require('../public/images/default_image.png');
+const iconPlus = require('../public/images/icon_plus_color.png');
+const iconMinus = require('../public/images/icon_minus_color.png');
 
 const HEADER_MAX_HEIGHT = 200;
-const HEADER_MIN_HEIGHT = 60;
+const HEADER_MIN_HEIGHT = 100;
 const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
 
 const style = {
@@ -37,14 +38,65 @@ const style = {
 		backgroundColor: '#000',
 		overflow: 'hidden',
 	},
-	scrollViewContent: {
-		marginTop: HEADER_MAX_HEIGHT,
-	},
 	backgroundImage: {
 		opacity: 0.8,
 		backgroundColor: 'rgba(0, 0, 0, 0.2)',
 		flex: 1,
 		height: HEADER_MAX_HEIGHT,
+	},
+	userInfo: {
+		backgroundColor: '#FFF',
+	},
+	wishHeader: {
+		paddingHorizontal: 6,
+		backgroundColor: '#FFF',
+	},
+	wishUser: {
+		flexDirection: 'row',
+	},
+	userImg: {
+		marginTop: -32,
+		marginRight: 6,
+		width: 64,
+		height: 64,
+		borderRadius: 32,
+		borderWidth: 3,
+		borderColor: '#FFF',
+	},
+	userName: {
+		color: '#34495E',
+		fontWeight: 'bold',
+		fontSize: 18,
+		height: 32,
+		lineHeight: 44,
+		marginBottom: 6,
+	},
+	wishText: {
+		color: '#34495E',
+		paddingLeft: 70,
+		marginTop: 10,
+		marginBottom: 20,
+	},
+	possibility: {
+		flexDirection: 'row',
+		borderTopColor: 'rgba(189, 195, 199, 0.2)',
+		borderBottomColor: 'rgba(189, 195, 199, 0.2)',
+		borderTopWidth: 1,
+		borderBottomWidth: 1,
+	},
+	posBtn: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 42,
+	},
+	posText: {
+		position: 'absolute',
+		width: '100%',
+		lineHeight: 42,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		color: '#91959E',
 	},
 };
 
@@ -53,6 +105,7 @@ export default class WishDetail extends Component {
 		super(props);
 		this.state = {
 			scrollY: new Animated.Value(0),
+			userInfoHeight: 0,
 			wish: props.navigation.state.params.wish,
 		};
 	}
@@ -80,7 +133,7 @@ export default class WishDetail extends Component {
 				>
 					<Comments
 						msgs={this.props.navigation.state.params.wish.msgs}
-						headerMaxHeight={HEADER_MAX_HEIGHT}
+						headerMaxHeight={HEADER_MAX_HEIGHT + this.state.userInfoHeight}
 					/>
 				</Animated.ScrollView>
 				<Animated.View
@@ -99,6 +152,31 @@ export default class WishDetail extends Component {
 						defaultSource={defaultSource}
 						source={{ uri: this.state.wish.img_url }}
 					/>
+					<View
+						style={style.userInfo}
+						onLayout={event => {
+							this.setState({
+								userInfoHeight: event.nativeEvent.layout.height,
+							});
+						}}
+					>
+						<View style={style.wishHeader}>
+							<View style={style.wishUser}>
+								<Image style={style.userImg} source={{ uri: this.state.wish.user.pic_url }} />
+								<Text style={style.userName}>{ this.state.wish.user.name }</Text>
+							</View>
+							<Text style={style.wishText}>{ this.state.wish.text }</Text>
+						</View>
+						<View style={style.possibility}>
+							<Text style={style.posText}>{ this.state.wish.possibility } %</Text>
+							<TouchableOpacity style={style.posBtn}>
+								<Image source={iconMinus} />
+							</TouchableOpacity>
+							<TouchableOpacity style={style.posBtn}>
+								<Image source={iconPlus} />
+							</TouchableOpacity>
+						</View>
+					</View>
 				</Animated.View>
 			</View>
 		);
